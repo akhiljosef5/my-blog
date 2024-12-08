7fff1c0d63b7ec4c55bf2c0f33699031c88c6d44
ajosef <ajosef@opentext.com>
2024-12-08 15:53:14 +0530

    outputting lists by iterating

--- src/home.jsx
+++ src/home.jsx
@@ -2,27 +2,20 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('akhil');
-    const [age, setAge] = useState(25);
-
-    const handleClick = (e) => {
-        setName('arya');
-        setAge(23);
-        console.log('hello ninjas', e);
-    }
-
-    const handleClickAgain = (name, e) => {
-        setName('akhil');
-        setAge(25);
-        console.log('hello ' + name, e.target);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <h1>{name} is {age} years old</h1>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('mario', e)}>Click me again</button>
+            {blogs.map((blog) => (
+                <div className="blogs-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>By: {blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
